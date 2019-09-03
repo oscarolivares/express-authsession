6aedba0cf34ee502f602daac12e10de23051a39c
Oscar Olivares <oscarolivares625@gmail.com>
2019-09-03 02:06:12 -0400

    Creation of the User model

---
+++ src/models/User.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+// User model
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      lowercase: true,
+      match: /\S+@\S+\.\S+/,
+      unique: true,
+      required: [true, 'email is required']
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: [true, 'password is required']
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user', 'guest'],
+      default: 'user'
+    },
+    firstName: String,
+    lastName: String
+  },
+  { timestamps: true }
+);
+
+// Encrypt password before save
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Aditional method for password comparations when user try to login
+userSchema.methods.verifyPassword = function(password, cb) {
+  bcrypt.compare(password, this.password, (err, match) => {
+    if (err) {
+      return cb(err);
+    } else {
+      return cb(null, match);
+    }
+  });
+};
+
+module.exports = mongoose.model('User', userSchema);
